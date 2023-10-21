a9b9de5b21e691b30ce15aebf983eaad7998cd2a
Rakib <mohammadmohiuddin3490@gmai.com>
2023-10-21 14:58:06 +0600

    relevent message when no product available of any brand

--- src/components/BrandProducts.jsx
+++ src/components/BrandProducts.jsx
@@ -1,21 +1,37 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import BrandProduct from "./BrandProduct";
 
 const BrandProducts = () => {
-    const productLoader = useLoaderData();
-    const {brand} = useParams();
-    const products = productLoader.filter(product => product.brand === brand)
-    
+  const productLoader = useLoaderData();
+  const { brand } = useParams();
+  const products = productLoader.filter((product) => product.brand === brand);
+
+  if (products.length < 1) {
     return (
-        <div>
-            <h2>product: {products.length}</h2>
-            <div className="container mx-auto p-8 md:p-16 lg:p-0 grid grid-cols-1 md:grid-cols-2 gap-12 mb-20">
-                {
-                    products.map(product => <BrandProduct key={product._id} product={product}></BrandProduct>)
-                }
-            </div>
+      <div className="card container mx-auto mt-48">
+        <figure>
+          <img src="https://i.ibb.co/gSk7Lsn/temporarily-out-stock-hanging-red-260nw-2124385799.jpg" />
+        </figure>
+        <div className="card-body">
+          <div className="card-actions justify-center">
+            <Link to="/">
+              <button className="btn btn-primary">Home</button>
+            </Link>
+          </div>
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <div className="container mx-auto p-8 md:p-16 lg:p-0 grid grid-cols-1 md:grid-cols-2 gap-12 my-20">
+        {products.map((product) => (
+          <BrandProduct key={product._id} product={product}></BrandProduct>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BrandProducts;
